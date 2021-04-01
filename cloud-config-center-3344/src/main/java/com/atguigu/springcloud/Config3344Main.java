f130fde71cf1c02d23763e592379a2d97e6d9bee
Your Name <you@example.com>
2021-04-01 12:28:27 +0800

    提交spring config

---
+++ cloud-config-center-3344/src/main/java/com/atguigu/springcloud/Config3344Main.java
@@ -0,0 +1,12 @@
+package com.atguigu.springcloud;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class Config3344Main {
+
+
+}
